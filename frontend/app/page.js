c97b3d515f84515f878f52c550223b407c31c2c8
Kinza Sheikh <75093626+KinzaaSheikh@users.noreply.github.com>
2024-05-07 14:53:27 +0500

    Updating layout

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,7 @@ import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     <main>
-      <NavBar />
+      <h1>Home page</h1>
     </main>
   );
 }
--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,11 +1,13 @@
-
+import AboutPage from '@/app/about/page';
+import Link from 'next/link';
 import React from 'react';
 
 function NavBar({item}) {
   return (
     <div>
-      <header className=" body-font">
-        
+      <header className="flex">
+        <h1>Nav</h1>
+        <Link href='/about'>About</Link>
       </header>
     </div>
   );
